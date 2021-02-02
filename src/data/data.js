9133aadec529b79b4ac04f7670cb2d07ec1cf553
MaurooRen <maurooren@gmail.com>
2021-02-02 01:09:05 -0500

    data set

---
+++ src/data/data.js
@@ -0,0 +1,35 @@
+const mra = {
+    name: 'Mauricio',
+    nickname: 'MaurooRen',
+    initials: 'mra',
+    email: 'maurooren@gmail.com',
+    resume: '',
+    socialMedia: [
+        {
+            name: 'GitHub',
+            link: 'https://github.com/maurooren',
+        },
+        {
+            name: 'GitHub',
+            link: 'https://github.com/maurooren',
+        },
+    ],
+    skills: {
+        frontEnd: ['React', 'REST API', 'Javascript', 'Git', 'HTML', 'CSS', 'Sass', 'Wordpress'],
+        design: ['User Interface', 'Website', 'Adobe XD', 'Photoshop', 'CorelDraw', 'Prints'],
+        others: ['Marketing', 'Social Media', 'SEO', 'Consulting', 'CMS'],
+    },
+    projects: [
+        {
+            id: 1,
+            title: 'GrupoMaris',
+            link: 'https://www.grupomaris.es/',
+            live: true,
+            short: 'Wordpress + Logo Design',
+            img: 'https://images.unsplash.com/photo-1606851449416-cb095ad60ded?ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1051&q=80'
+        },
+    ],
+    
+}
+
+export default mra;
