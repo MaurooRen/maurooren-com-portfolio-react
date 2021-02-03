ac2723954c0b1071c6ccfb9ac99704fc97493556
MaurooRen <maurooren@gmail.com>
2021-02-02 20:28:36 -0500

    favicon added and trash files removed

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
     return (
         <header className="Header">
             <div className="container w-container">
-                <h1>mra.</h1>
+                <h1>maurooren.</h1>
                 <nav className="mainNav">
                     <ul>
                         <li>
